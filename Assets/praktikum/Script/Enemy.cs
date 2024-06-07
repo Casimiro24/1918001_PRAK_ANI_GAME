f79ff9098b2c9caf5328be07936fe10f681a6dd5
Casimiro24 <thiocasimiro@gmail.com>
2024-06-08 01:05:57 +0700

    ENEMY AI & ATTACK

---
+++ Assets/praktikum/Script/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    Warrior wr;
+    // Use this for initialization
+    void Start()
+    {
+        //Warrior adl nama dari game object yg ada di hierarchy
+        wr = GameObject.Find("Warrior").GetComponent<Warrior>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.transform.tag == "Player")
+        {
+            wr.nyawa--;
+        }
+        if (wr.nyawa < 0)
+        {
+            wr.play_again = true;
+        }
+    }
+}
+
